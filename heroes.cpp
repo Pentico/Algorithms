f21a6b366fd227e4c5f7c1984168562d9ea7643b
Pentico <morulaneat@gmail.com>
2016-09-12 02:21:39 +0200

    Initial commit

---
+++ heroes.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tuane on 2016/08/26.
+//
+namespace Heroes {
+
+    const int H = 10;
+
+    
+}
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "heroes.cpp" // checking the namespace definition.
+
+int dataEgnore();
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << Heroes::H; // using namespace Heroes
+    return 0;
+}
+
+
+//////////////// functions definitions ////////
+
+int dataEngore(){
+
+
+    return NULL;
+}
+
