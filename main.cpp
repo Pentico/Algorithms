2d6b35a54d85b5a210b3194f4559861624ded296
Pentico <morulaneat@gmail.com>
2016-09-17 23:02:09 +0200

    simply recursion

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include "heroes.cpp" // checking the namespace definition.
 
-int dataEgnore();
+int dataEgnore(int);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << Heroes::H; // using namespace Heroes
+    std::cout << dataEgnore(10) <<std::endl;
     return 0;
 }
 
 
 //////////////// functions definitions ////////
 
-int dataEngore(){
+int dataEgnore( int tempNumb ){
 
+    if(tempNumb == 0){
+        return 0;
+    }else{
+        return dataEgnore(tempNumb-1) +tempNumb;
+    }
 
     return NULL;
 }
